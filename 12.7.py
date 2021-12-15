73e97768a26f09e9992a749d77ca3aaf9c37a6b2
Danil <86056760+comelovedo@users.noreply.github.com>
2021-12-15 13:38:00 +0100

    Two last tasks

---
+++ 12.7.py
@@ -0,0 +1,15 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = float(input("Input amount of deposit: "))
+deposit = [round(float(money * i / 100), 2) for i in list(per_cent.values())]
+print(deposit)
+print("Your deposit:", (money))
+print("Maximum profit for your deposit", deposit)
+# per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+# money = float(input("Deposit amount:"))
+# deposit = []
+# for i in list(per_cent.values()):
+#    deposit.append(float(money * i / 100))
+# deposit = [round(float(money * i / 100), 2) for i in list(per_cent.values())]
+#
+# print("Your deposit:", (money))
+# print("Maximum profit for your deposit", deposit)
---
+++ 13.8.py
@@ -0,0 +1,16 @@
+ticket = int(input("Please input amount of tickets:"))
+price = []
+for i in range(1, ticket+1): # For i in range from 1 to 999+, cause the tickets have to be more than 0
+    age = int(input(f"Input the age {i} of customer: "))
+    if age in range(0, 18):
+        price.append(0)
+    elif age in range(18, 25):
+        price.append(990)
+    elif age in range(25, 110):
+        price.append(1390)
+    else:
+        print("Invalid value")
+    if ticket > 3: # Discount in 10% if amount of tickets > 3
+        print("Amount to pay with a 10% discount", sum(price) - ((sum(price) / 100) * 10), "rubles")
+    else:
+        print("Amount to pay:", sum(price), "rubles")
