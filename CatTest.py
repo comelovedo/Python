03cb085db76800a0d2a8266a60c303cfac61fa89
Danil <86056760+comelovedo@users.noreply.github.com>
2022-01-04 17:05:55 +0100

    Cats

--- CatTest.py
+++ CatTest.py
@@ -2,17 +2,21 @@ import json
 
 from cat import Cat
 
-
-def check_cat(elem, item):
-    if elem[item]['code'] == 'cat' in templates:
-        return True
-
-
 with open(r'AnimalDict.json', encoding='utf8') as AnimalD:
     templates = json.load(AnimalD)
 
+
+def check_cat():
+    if "Cat" in templates.elem():
+        return True
+    else:
+        print("There are no one cats here.")
+
+
 for elem in templates['results']:
-    for item in elem:
-        if item == 'species' and check_cat(elem, item):
+    if elem.get('species') == 'cat':
+        if elem == 'species' and check_cat:
             obj_Cat = Cat(elem['name'], elem['gender']['name'], elem['age'])
             print(obj_Cat.name)
+
+
