d48e4e8377b2e9f042b3b03ec446b14222e09149
Danil <86056760+comelovedo@users.noreply.github.com>
2022-01-06 17:08:36 +0100

    task 16.9

--- 16.9/16.9.3.py
+++ 16.9/16.9.3.py
@@ -1,4 +1,17 @@
-#
+# Need to write a program that processes data,
+# and get the following at the console output: Client "...". Balance: "..." rub.
 
 class E_Wallet:
-    def__init__
+    def __init__(self, name="", balance=0):
+        self.name = name
+        self.balance = balance
+
+    def __str__(self):
+        return f"Customer: {self.name}, Balance: {self.balance} rub."
+
+
+a = str(input("Input your name: "))
+b = int(input("Available amount: "))
+
+Customer_1 = E_Wallet(a, b)
+print(Customer_1)
