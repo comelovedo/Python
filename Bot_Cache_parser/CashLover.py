2373946d3236bba1529282099f6683100518fe6c
Danil <86056760+comelovedo@users.noreply.github.com>
2022-01-25 21:21:35 +0100

    25.01.22

--- Bot_Cache_parser/CashLover.py
+++ Bot_Cache_parser/CashLover.py
@@ -37,25 +37,25 @@
 # bot.polling(none_stop=True)
 
 
-# import telebot
-#
-# TOKEN = "5263070884:AAGRUPhMy68le_bYaj_9XHMyUB8DlMFmdLY"
-# bot = telebot.TeleBot(TOKEN)
-#
-#
-# # Обрабатываются все сообщения, содержащие команды '/start' or '/help'.
-# @bot.message_handler(commands=['start', 'help'])
-# def handle_start_help(message: telebot.types.Message):
-#     bot.reply_to(message, f"Иди ты в баню, {message.chat.username}")
-#
-#
-# # Напишите обработчик, который на сообщения с фотографией будет отвечать сообщением «Nice meme XDD»
-# @bot.message_handler(content_types=['photo', ])
-# def say_lmao(message: telebot.types.Message):
-#     bot.reply_to(message, 'Nice meme XDD')
-#
-#
-# bot.polling(none_stop=True)
+import telebot
+
+TOKEN = "5263070884:AAGRUPhMy68le_bYaj_9XHMyUB8DlMFmdLY"
+bot = telebot.TeleBot(TOKEN)
+
+
+# Обрабатываются все сообщения, содержащие команды '/start' or '/help'.
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message: telebot.types.Message):
+    bot.reply_to(message, f"Иди ты в баню, {message.chat.username}")
+
+
+# Напишите обработчик, который на сообщения с фотографией будет отвечать сообщением «Nice meme XDD»
+@bot.message_handler(content_types=['photo', ])
+def say_lmao(message: telebot.types.Message):
+    bot.reply_to(message, 'Nice meme XDD')
+
+
+bot.polling(none_stop=True)
 
 
 
