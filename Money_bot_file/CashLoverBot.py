ded95ee2a5dd5b2b55e1df18dc1de531c88fe76f
comelovedo <Bazucer@gmail.com>
2022-01-27 22:54:35 +0500

    telegram bot

--- Money_bot_file/CashLoverBot.py
+++ Money_bot_file/CashLoverBot.py
@@ -1,62 +1,47 @@
-# Телеграм Бот
+import telebot
+from Config import keys, Currencies, TOKEN
+from Extentions import CryptoConvector, APIException
 
-# Content types:
-# audio — аудиозапись;
-# photo — фотография, картинка;
-# voice — голосовое сообщение;
-# video — видеозапись;
-# video_note - видео заметки
-# document — документ;
-# text — текстовое сообщение;
-# location — геолокация;
-# contact — контакт;
-# sticker — стикер.
-# new_chat_member,
-# new_chat_photo
-# delete_chat_photo
-# group_chat_created,
-# supergroup_chat_created
-# channel_chat_created,
-# migrate_to_chat_id
-# migrate_from_chat_id
-# pinned_message
+bot = telebot.TeleBot(TOKEN)
 
 
-# import telebot
-#
-# TOKEN = "5263070884:AAGRUPhMy68le_bYaj_9XHMyUB8DlMFmdLY"
-#
-# bot = telebot.TeleBot(TOKEN)
-#
-#
-# @bot.message_handler(content_types=['voice'])
-# def repeat(message: telebot.types.Message):
-#     bot.send_message(message.chat.id, "Обернись, сзади Досманова!")
-#
-#
-# bot.polling(none_stop=True)
+# Обрабатываются все сообщения, содержащие команды '/start' or '/help'.
+@bot.message_handler(commands=['start', 'help'])
+def send_help(message: telebot.types.Message, ):
+    text = 'Чтобы начать работу, введите комманду в следующем формате: \b<Какую валюту хотите конвектировать>  \
+     <В какую валюту хотите конвектировать> \
+     <Сумма конвектируемой валюты\nУвидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
 
 
-# import telebot
-#
-# TOKEN = "5263070884:AAGRUPhMy68le_bYaj_9XHMyUB8DlMFmdLY"
-# bot = telebot.TeleBot(TOKEN)
-#
-#
-# # Обрабатываются все сообщения, содержащие команды '/start' or '/help'.
-# @bot.message_handler(commands=['start', 'help'])
-# def handle_start_help(message: telebot.types.Message):
-#     bot.reply_to(message, f"Иди ты в баню, {message.chat.username}")
-#
-#
-# # Напишите обработчик, который на сообщения с фотографией будет отвечать сообщением «Nice meme XDD»
-# @bot.message_handler(content_types=['photo', ])
-# def say_lmao(message: telebot.types.Message):
-#     bot.reply_to(message, 'Nice meme XDD')
-#
-#
-# bot.polling(none_stop=True)
+# По комманде value  выдает все допустимые валюты
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in Currencies.keys():
+        text = '\n'.join((text, key,))
+    bot.reply_to(message, text)
 
 
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    values = message.text.split(' ')
+    try:
+        if len(values) != 3:
+            raise APIException("Слишком много параметров либо некорректные данные попробуйте /values для "
+                               "уточнения параметров и /help для помощи")
+
+        # quote - валюта которую хотим конвектировать base - валюта в которую хотим конвектировать, amount - кол-во
+        # валюты
+        quote, base, amount = values
+        total_base = CryptoConvector.get_price(quote, base, amount)
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n {e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать комманду\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
 
 
+bot.polling(none_stop=True)
